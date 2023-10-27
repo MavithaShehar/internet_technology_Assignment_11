eeecca3ce84eed470ec0f9d053041de6a9762e07
MavithaShehar <mavithashehar@gmail.com>
2023-10-27 18:30:24 +0530

    upload item page and bug fix

--- Assignment_11/controller/itemController.js
+++ Assignment_11/controller/itemController.js
@@ -1,19 +1,16 @@
-import {ItemModel} from "../model/itemModel.js";
-import {items_db} from "../db/db.js";
+import { ItemModel } from "../model/itemModel.js";
+import { items_db } from "../db/db.js";
 
-// clean inputs
+// Clean inputs
 const cleanInputs = () => {
-
     $('#items_id').val('');
     $('#items_name').val('');
     $('#items_qty').val('');
     $('#items_price').val('');
-
 };
 
-// load item
+// Load items
 const loadItems = () => {
-
     $('#items-tbl-body').empty();
 
     items_db.map((item, index) => {
@@ -25,14 +22,10 @@ const loadItems = () => {
                         </tr>`;
         $('#items-tbl-body').append(tbl_row);
     });
-
 };
 
 // Add item
 $('#items-btns>button').eq(0).on('click', () => {
-
-    console.log("hello Item");
-
     let items_id = $('#items_id').val();
     let items_name = $('#items_name').val();
     let items_qty = $('#items_qty').val();
@@ -40,15 +33,13 @@ $('#items-btns>button').eq(0).on('click', () => {
 
     let items = new ItemModel(items_id, items_name, items_qty, items_price);
     items_db.push(items);
-    console.log("hello Item 02 ");
 
     cleanInputs();
-    loadItems(); // call load customer function
+    loadItems();
 });
 
-// update item
-$("#items-btns-btns").eq(1).on("click", () => {
-
+// Update item
+$('#items-btns>button').eq(1).on('click', () => {
     let items_id = $('#items_id').val();
     let items_name = $('#items_name').val();
     let items_qty = $('#items_qty').val();
@@ -56,22 +47,23 @@ $("#items-btns-btns").eq(1).on("click", () => {
 
     let items_obj = new ItemModel(items_id, items_name, items_qty, items_price);
 
-    // find item index
+    // Find item index
     let index = items_db.findIndex(item => item.items_id === items_id);
 
-    // update item in the db
-    items_db[index] = items_obj;
+    if (index !== -1) {
+        // Update the item in the database
+        items_db[index] = items_obj;
 
-    // clear();
-    $("#items-btns>button").click();
+        // Clear the input fields
+        cleanInputs();
 
-    // load customer data
-    loadItems();
-})
+        // Reload items
+        loadItems();
+    }
+});
 
-// delete
-$("#items-btns>button").eq(2).on("click", () => {
-
+// Delete item
+$('#items-btns>button').eq(2).on('click', () => {
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
@@ -82,35 +74,33 @@ $("#items-btns>button").eq(2).on("click", () => {
         confirmButtonText: 'Yes, delete this!'
     }).then((result) => {
         if (result.isConfirmed) {
-
             let items_id = $("#items_id").val();
-
-            // find item index
             let index = items_db.findIndex(item => item.items_id === items_id);
 
-            // remove the item from the db
-            item_db.splice(index, 1);
+            if (index !== -1) {
+                // Remove the item from the database
+                items_db.splice(index, 1);
 
-            Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-            )
+                Swal.fire(
+                    'Deleted!',
+                    'Your item has been deleted.',
+                    'success'
+                );
 
-            $("#items-btns>button").click();
+                // Clear the input fields
+                cleanInputs();
 
-            // load student data
-            loadItems();
+                // Reload items
+                loadItems();
+            }
         }
-    })
-})
+    });
+});
 
-// fill item
-$('#item-btns-body').on('click', 'tr' , function() {
-    let index = $(this).index();
-
+// Fill item
+$('#items-tbl-body').on('click', 'tr', function () {
     let items_id = $(this).find('.items_id').text();
-    let items_name= $(this).find('.items_name').text();
+    let items_name = $(this).find('.items_name').text();
     let items_qty = $(this).find('.items_qty').text();
     let items_price = $(this).find('.items_price').text();
 
@@ -118,33 +108,25 @@ $('#item-btns-body').on('click', 'tr' , function() {
     $('#items_name').val(items_name);
     $('#items_qty').val(items_qty);
     $('#items_price').val(items_price);
-
-    console.log("items_id: ", items_id);
 });
 
-
-// serch customer
-
+// Search items
 $('#items-search').on('input', () => {
-    let items_search = $('#items-search').val();
+    let items_search = $('#items-search').val().toLowerCase();
 
-    let results = items_db.filter((item) =>
-
-        item.items_name.toLowerCase().startsWith(search_term.toLowerCase()) ||
-        item.items_id.toLowerCase().startsWith(search_term.toLowerCase())
-
+    let results = items_db.filter(item =>
+        item.items_name.toLowerCase().includes(items_search) ||
+        item.items_id.toLowerCase().includes(items_search)
     );
 
-    console.log(results);
-
     $('#items-tbl-body').empty();
-    results.map((item, index) => {
+    results.forEach(item => {
         let tbl_row = `<tr>
                         <td class="items_id">${item.items_id}</td>
                         <td class="items_name">${item.items_name}</td>
                         <td class="items_qty">${item.items_qty}</td>
-                        <td class="items_price">${item.items_price}</td></tr>`;
+                        <td class="items_price">${item.items_price}</td>
+                        </tr>`;
         $('#items-tbl-body').append(tbl_row);
     });
-
 });
